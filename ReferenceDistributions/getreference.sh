3878b2bd1b99a6fd0ab406067fbed9eb0e17ba65
Andras Hartmann <andras.hartmann@uni.lu>
2017-08-01 15:00:10 +0200

    Script to retrieve reference distributions

---
+++ ReferenceDistributions/getreference.sh
@@ -0,0 +1,3 @@
+wget https://webdav-r3lab.uni.lu/public/cbg/RefBool_ReferenceDistributions/DataAccess.txt
+wget https://webdav-r3lab.uni.lu/public/cbg/RefBool_ReferenceDistributions/Thresholds_CoTFcRF.mat
+wget https://webdav-r3lab.uni.lu/public/cbg/RefBool_ReferenceDistributions/background_genes.txt
