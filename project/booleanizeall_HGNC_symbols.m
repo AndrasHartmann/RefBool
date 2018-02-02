a4302ad169e7b7d6e00dbf933df79947823d883d
Andras Hartmann <andras.hartmann@uni.lu>
2018-02-02 10:51:56 +0100

    New booleanization script with HGNC symbols

---
+++ project/booleanizeall_HGNC_symbols.m
@@ -0,0 +1,81 @@
+addpath ../src/
+
+%% Get all files to booleanize
+!ls ~/Work/LCSB_EPI_CBG/Data/HighLowEffi/*/*Expression.txt > x.tmp
+%!ls ~/Work/LCSB_EPI_CBG/Data/Input/*/*EXPRESSION.txt > x.tmp
+%!ls ~/Work/LCSB_EPI_CBG/Data/Differentiation_Examples/*/*EXPRESSION.txt > x.tmp
+%!ls ../*EXPRESSION.txt > x.tmp
+
+
+%%Initialize background
+background_genes = readtable('../ReferenceDistributions/background_genes.txt', ...,
+      'delimiter','\t', 'ReadVariableNames',false );
+
+gNames = background_genes.(2);
+
+load ../ReferenceDistributions/Thresholds_CoTFcRF.mat;
+
+%% Do the actual booleanization
+fid = fopen('x.tmp','rt');
+while true
+     thisline = fgetl(fid);
+     if ~ischar(thisline); 
+        break; 
+     end  %end of file
+
+
+     Expr = readtable(thisline,'delimiter', '\t');
+     %align expression to dists
+     indices = -ones(size(gNames));
+     %Gene_ID = cell(size(gNames)); 
+     for j = 1:length(indices)
+        expind = find(ismember(Expr.gene_id,gNames{j}));
+        if ~isempty(expind)
+           indices(j)=expind;
+        end
+     end
+
+     %removing non-existing indices
+     mydists = dists;
+     toDel = (indices==-1);
+
+     mydists(toDel)=[];
+
+     indices(toDel)=[];
+
+
+     %vector for expression
+     expression = Expr.TPM(indices);
+     HGNC_symbol = Expr{indices,1};
+
+     %p-values
+    [ Pvals_low,Pvals_up,Pvals_inter_low,Pvals_inter_up,Pvals_low_adj,Pvals_up_adj,Pvals_inter_low_adj,Pvals_inter_up_adj ] = CalculatePValues( mydists, expression); 
+
+    % booleanizing everything
+     bool_value_all_low = Pvals_inter_low_adj < 0.95;
+     %explicitely set it to 0 whenever the value is 0
+     %TODO: is there a better way to do this?
+     bool_value_all_low (expression==0) = 0;
+
+      % booleanizing everything
+     bool_value_all = Pvals_inter_up_adj > 0.05;
+     %explicitely set it to 0 whenever the value is 0
+     %TODO: is there a better way to do this?
+     bool_value_all (expression==0) = 0;
+     
+     %only lower and upper 10%
+     bool_value = nan(size(Pvals_inter_low_adj));
+
+     bool_value (Pvals_low_adj < 0.1) = 0;
+     bool_value (Pvals_up_adj < 0.1) = 1;
+     
+     output = table(HGNC_symbol, expression, bool_value_all, bool_value_all_low, bool_value);
+     %background_genes.Properties.VariableNames = {'Gene_ID', 'HGNC_symbol'};
+     %output = innerjoin(background_genes,output);
+
+     %Write file
+     [pathstr,name,ext] = fileparts(thisline)
+     writetable(output,[pathstr filesep name '_bool' ext], 'Delimiter','\t');
+end
+fclose(fid);
+%!rm x.tmp
